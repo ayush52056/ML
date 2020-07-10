7b6af5f34f4e18492ad8c05d170bd2ab19e663e6
Ayush Singh <52326756+ayush52056@users.noreply.github.com>
2020-07-10 22:40:20 +0530

    ab tests

--- backend/server/apps/endpoints/models.py
+++ backend/server/apps/endpoints/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Endpoint(models.Model):
     '''
     The Endpoint object represents ML API endpoint.
+
     Attributes:
         name: The name of the endpoint, it will be used in API URL,
         owner: The string with owner name,
@@ -15,6 +16,7 @@ class Endpoint(models.Model):
 class MLAlgorithm(models.Model):
     '''
     The MLAlgorithm represent the ML algorithm object.
+
     Attributes:
         name: The name of the algorithm.
         description: The short description of how the algorithm works.
@@ -35,6 +37,7 @@ class MLAlgorithm(models.Model):
 class MLAlgorithmStatus(models.Model):
     '''
     The MLAlgorithmStatus represent status of the MLAlgorithm which can change during the time.
+
     Attributes:
         status: The status of algorithm in the endpoint. Can be: testing, staging, production, ab_testing.
         created_by: The name of creator.
@@ -51,6 +54,7 @@ class MLAlgorithmStatus(models.Model):
 class MLRequest(models.Model):
     '''
     The MLRequest will keep information about all requests to ML algorithms.
+
     Attributes:
         input_data: The input data to ML algorithm in JSON format.
         response: The response of the ML algorithm in JSON format.
@@ -64,3 +68,25 @@ class MLRequest(models.Model):
     feedback = models.CharField(max_length=10000, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     parent_mlalgorithm = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE)
+
+
+class ABTest(models.Model):
+    '''
+    The ABTest will keep information about A/B tests.
+    Attributes:
+        title: The title of test.
+        created_by: The name of creator.
+        created_at: The date of test creation.
+        ended_at: The date of test stop.
+        summary: The description with test summary, created at test stop.
+        parent_mlalgorithm_1: The reference to the first corresponding MLAlgorithm.
+        parent_mlalgorithm_2: The reference to the second corresponding MLAlgorithm.
+    '''
+    title = models.CharField(max_length=10000)
+    created_by = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    ended_at = models.DateTimeField(blank=True, null=True)
+    summary = models.CharField(max_length=10000, blank=True, null=True)
+
+    parent_mlalgorithm_1 = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE, related_name="parent_mlalgorithm_1")
+    parent_mlalgorithm_2 = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE, related_name="parent_mlalgorithm_2")
--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(-!($@w=e6^52aoj!1zn^ljp-cunj+ug_90q@@2675(e@)t^(2'
+SECRET_KEY = '^lw0)9982b8nt^^-t34967jnl(n7iojl!q+!5xojw9mr6u0h9f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', # add django rest framework
-    #apps
+    # apps
     'apps.endpoints',
-    'apps.ml',
+    'apps.ml'
 ]
 
 MIDDLEWARE = [
@@ -121,4 +121,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
